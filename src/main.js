7abefacb745ed882b53b7d758da95adbaa026fb4
Hardyguardy <fighter2007max@gmail.com>
2025-11-05 23:00:51 +0100

    Add show more button expand function

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
 import "./index.css";
+
+// Функция для Show More
+function toggleTextBlock(button) {
+  const container = button.closest(".relative").parentElement;
+  const textContent = container.querySelector(".text-content");
+  const gradientOverlay = container.querySelector(".gradient-overlay");
+
+  const isExpanded = textContent.style.maxHeight !== "100px";
+
+  if (isExpanded) {
+    // Сворачивание кнопки
+    textContent.style.maxHeight = "100px";
+    gradientOverlay.style.opacity = "1";
+    button.textContent = "Show more";
+  } else {
+    // Разворачивание кнопки
+    textContent.style.maxHeight = textContent.scrollHeight + "px";
+    gradientOverlay.style.opacity = "0";
+    button.textContent = "Show less";
+  }
+}
+
+window.toggleTextBlock = toggleTextBlock;
